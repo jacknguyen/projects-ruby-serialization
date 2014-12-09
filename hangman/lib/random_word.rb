980bcd2499004fcaaedb5b6c0adfa8232c31d747
Jack Nguyen <nguyen.jack@gmail.com>
2014-12-08 22:59:18 -0800

    move random word gen into it's own library

---
+++ hangman/lib/random_word.rb
@@ -0,0 +1,24 @@
+require 'open-uri'
+require 'nokogiri'
+
+page_url = "http://scrapmaker.com/view/twelve-dicts/5desk.txt"
+
+# retrieves the words from the site
+@page = Nokogiri::HTML(open(page_url)).css('pre').to_s
+
+def clean_words(html)
+  arr_of_words = html.split(/(>[a-z]+<)/).find_all { |x| x.match(/(>[a-z]+<)/) }
+
+  arr_of_words.each do |word|
+    word.gsub!(/\>|\</, '')
+  end
+
+  arr_of_words
+end
+
+def random_word
+  list_of_words = clean_words(@page)
+
+  # find a random word that's between 5 and 12 characters in length
+  list_of_words.find_all { |word| word.length > 5 && word.length < 12 }.sample
+end
